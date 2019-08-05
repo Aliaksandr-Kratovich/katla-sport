ea29de0a2edc85cc760627f184325008d2d1107b
Aliaksandr Kratovich <Aliaksandr_Kratovich@epam.com>
2019-08-05 18:24:37 +0300

    Merge step10

--- KatlaSportNg/src/app/hive-management/lists/hive-section-list.component.ts
+++ KatlaSportNg/src/app/hive-management/lists/hive-section-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HiveSectionListItem } from '../models/hive-section-list-item';
 import { HiveService } from '../services/hive.service';
+import { HiveSectionService } from '../services/hive-section.service';
 
 @Component({
   selector: 'app-hive-section-list',
@@ -16,6 +17,7 @@ export class HiveSectionListComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
+    private hiveSectionService: HiveSectionService,
     private hiveService: HiveService
   ) { }
 
@@ -25,4 +27,14 @@ export class HiveSectionListComponent implements OnInit {
       this.hiveService.getHiveSections(this.hiveId).subscribe(s => this.hiveSections = s);
     })
   }
+  onDelete(hiveSectionId: number) {
+    var hiveSection = this.hiveSections.find(h => h.id == hiveSectionId);
+    this.hiveSectionService.setHiveSectionStatus(hiveSectionId, true).subscribe(c => hiveSection.isDeleted = true);
+  }
+
+  onUndelete(hiveSectionId: number) {
+    var hiveSection = this.hiveSections.find(h => h.id == hiveSectionId);
+    this.hiveSectionService.setHiveSectionStatus(hiveSectionId, false).subscribe(c => hiveSection.isDeleted = false);
+  }
+
 }
